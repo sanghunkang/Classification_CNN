a52419c92ba0762449de46a120aa87482d94d9a1
sanghunKang <sanghunkang.dev@gmail.com>
2017-06-01 13:53:29 +0900

    minor revision

--- customhelpers/process_data/image_handler.py
+++ customhelpers/process_data/image_handler.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#############################################################################
-import random as random
 
-import _pickle as cPickle
-import matplotlib.pyplot as plt
 import numpy as np
-import PIL as pil
 import PIL.Image as Image
 
-def open_image(path_img):
-	ret = Image.open(path_img)
-	return ret
+# def open_image(path_img):
+# 	return Image.open(path_img)
 
 def crop_innersqr(img_open):
 	size_img = img_open.size
 	min_side = min(size_img)
 	padding_h, padding_v = (size_img[0] - min_side)/2, (size_img[1] - min_side)/2
-	ret = img_open.crop((padding_h, padding_v, size_img[0] - padding_h, size_img[1] - padding_v))
-	return ret
+	return img_open.crop((padding_h, padding_v, size_img[0] - padding_h, size_img[1] - padding_v))
 
 def reshape_image(img_open, shape):
-	ret = img_open.resize(shape)
-	return ret
+	return img_open.resize(shape) 
 
 def serialize_image(img_open):
 	img_serialized = np.asarray(img_open)
-	ret = img_serialized.reshape(1, img_serialized.size)[0]
-	return ret
+	return img_serialized.reshape(1, img_serialized.size)[0]
 
 def process_image(path_img, resolution):
-	img_open = open_image(path_img)
+	img_open = Image.open(path_img)
 	img_cropped = crop_innersqr(img_open)
-	img_reshaped = reshape_image(img_cropped, resolution)
-	ret = serialize_image(img_reshaped)		
-	return ret
-
+	img_reshaped = img_cropped.resize(resolution) 
+	# img_reshaped = reshape_image(img_cropped, resolution)
+	return serialize_image(img_reshaped)		
--- customhelpers/process_data/kaggle_catdog_128.py
+++ customhelpers/process_data/kaggle_catdog_128.py
@@ -5,10 +5,8 @@
 import os, sys
 
 # Import 3rd party packages
-import _pickle as cPickle
 import pickle as pickle
 import numpy as np
-import PIL.Image as Image
 
 from image_handler import process_image
 
@@ -27,10 +25,8 @@ def make_pickle(data_input, path_file):
 		pickle.dump(data_input, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 direc = get_direc_data_processed(SYSTEM)
-
 seq_pickle = np.zeros(shape=(20000, 64*64*3 + 1))
 
-isIndex = True
 for i in range(0,10000): 
 	path_img = direc + "\\train\\cat." + str(i) + ".jpg"
 	img_processed =  process_image(path_img, (64,64))
@@ -45,42 +41,17 @@ for j in range(0,10000):
 
 make_pickle(seq_pickle, direc + "\\kaggle_catdog_train_64x64_TEST.pickle")
 
-# for i in range(0,2):
-# 	try: 
-# 		kaggle_catdog.open_image(direc + "test\\cat." + str(10000+i) + ".jpg")
-# 		size_img = kaggle_catdog.image.size
-# 		min_side = min(size_img)
-# 		padding_h = (size_img[0] - min_side)/2
-# 		padding_v = (size_img[1] - min_side)/2
-# 		kaggle_catdog.image.crop((padding_h, padding_v, size_img[0] - padding_h, size_img[1] - padding_v))
+for i in range(0,2500):
+	path_img = direc + "test\\cat." + str(10000+i) + ".jpg"
+	img_processed =  process_image(path_img, (64,64))
+	seq_pickle[i] = np.append(img_processed, [3])
+	print(i)
 
-# 		kaggle_catdog.adjust_size((64,64))
-# 		kaggle_catdog.serialize_image2()
-# 		saechi_ser = kaggle_catdog.arr_img
+for j in range(0,2500):
+	path_img = direc + "test\\dog." + str(10000+j) + ".jpg"
+	img_processed =  process_image(path_img, (64,64))
+	seq_pickle[i] = np.append(img_processed, [5])
+	seq_pickle[2500+j] = saechi_ser
+	print(j)
 
-# 		saechi_ser = np.append(saechi_ser, [3])
-# 		seq_pickle[i] = saechi_ser
-# 	except FileNotFoundError:
-# 		isIndex = False
-# 	# i += 1
-# 	print(i)
-
-# for j in range(0,2):
-# 	try: 
-# 		kaggle_catdog.open_image(direc + "test\\dog." + str(10000+j) + ".jpg")
-# 		size_img = kaggle_catdog.image.size
-# 		min_side = min(size_img)
-# 		padding_h = (size_img[0] - min_side)/2
-# 		padding_v = (size_img[1] - min_side)/2
-# 		aa = kaggle_catdog.image.crop((padding_h, padding_v, size_img[0] - padding_h, size_img[1] - padding_v))
-
-# 		kaggle_catdog.adjust_size((64,64))
-# 		kaggle_catdog.serialize_image2()
-# 		saechi_ser = kaggle_catdog.arr_img
-
-# 		saechi_ser = np.append(saechi_ser, [5])
-# 		seq_pickle[2500+j] = saechi_ser
-# 	except FileNotFoundError:
-# 		isIndex = False
-# 	# j += 1
-# 	print(j)
+make_pickle(seq_pickle, direc + "\\kaggle_catdog_test_64x64_TEST.pickle")
